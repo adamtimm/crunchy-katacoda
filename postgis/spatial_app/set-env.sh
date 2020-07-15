d0db1e2a3301a1ff66f5ab580a41ec180bc907b6
adamtimm <amtimm@gmail.com>
2020-07-15 14:56:08 -0500

    fixing pieces of the app

--- postgis/spatial_app/set-env.sh
+++ postgis/spatial_app/set-env.sh
@@ -4,7 +4,7 @@
 
 echo 'please wait while we prep the environment (should take about 15 seconds)'
 echo 'starting the database'
-docker run -d -p 5432:5432 -e PG_USER=groot -e PG_PASSWORD=password -e PG_DATABASE=firedata --name=pgsql crunchydata/crunchy-postgres-appdev
+docker run -d -p 5432:5432 -e PG_USER=groot -e PG_PASSWORD=password -e PG_DATABASE=firedata -e PG_ROOT_PASSWORD=root --name=pgsql crunchydata/crunchy-postgres-appdev
 
 until PGPASSWORD="password" psql -h localhost -U groot postgres -c '\l' &> /dev/null; do
   echo >&2 "$(date +%Y%m%dt%H%M%S) Waiting for Postgres to start"
@@ -12,7 +12,7 @@ until PGPASSWORD="password" psql -h localhost -U groot postgres -c '\l' &> /dev/
 done
 
 echo 'loading data'
-curl https://app.box.com/s/e6ubh3dw1k8yhp76v3i9rnvxymse0uxl |gzip -dc | PGPASSWORD="password" psql -h localhost -U groot firedata
+curl https://app.box.com/s/e6ubh3dw1k8yhp76v3i9rnvxymse0uxl |gzip -dc | PGPASSWORD="root" psql -h localhost -U postgres -d firedata
 
 clear
 : 'Ready to go'
